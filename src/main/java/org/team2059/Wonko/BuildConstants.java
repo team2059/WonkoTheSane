94873ec35ea0a698a5185863c0bd876cb71a81ea
Ryan Fortner <ryankfortner@gmail.com>
2025-03-21 18:27:35 -0400

    Updated WPIlib

--- src/main/java/org/team2059/Wonko/BuildConstants.java
+++ src/main/java/org/team2059/Wonko/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "WonkoTheSane";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "74e3707a5aa69ee717ac6e89ef12839c776c299c";
-  public static final String GIT_DATE = "2025-03-19 21:20:52 EDT";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "957f9d427ade09b451271b5fb0d981a2c9389cf6";
+  public static final String GIT_DATE = "2025-03-20 20:26:55 EDT";
   public static final String GIT_BRANCH = "post-wake-changes";
-  public static final String BUILD_DATE = "2025-03-20 20:22:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1742516536086L;
+  public static final String BUILD_DATE = "2025-03-21 18:21:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1742595708088L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
