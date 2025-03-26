b1af0f281f6822486ada2d71f20cbcf845539822
Ryan Fortner <ryankfortner@gmail.com>
2025-03-26 18:50:40 -0400

    Fix URCL setup

--- src/main/java/org/team2059/Wonko/BuildConstants.java
+++ src/main/java/org/team2059/Wonko/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "WonkoTheSane";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 57;
-  public static final String GIT_SHA = "a7111ac25ed47c2a63dfe7ca1b7cc4d0b59bf729";
-  public static final String GIT_DATE = "2025-03-23 20:42:06 EDT";
+  public static final int GIT_REVISION = 58;
+  public static final String GIT_SHA = "119a431ab5459e4042b9b54e670d253bfc7210d8";
+  public static final String GIT_DATE = "2025-03-26 18:44:14 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-03-26 18:22:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1743027763193L;
+  public static final String BUILD_DATE = "2025-03-26 18:48:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1743029326262L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
