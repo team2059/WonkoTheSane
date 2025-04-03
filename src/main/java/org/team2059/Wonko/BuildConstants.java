88d8050dafad001acd004a5c1d86dc3c7804975f
Aarush Jain <156387912+ajain189@users.noreply.github.com>
2025-04-03 19:40:31 -0400

    left paths and autos (#26)

--- src/main/java/org/team2059/Wonko/BuildConstants.java
+++ src/main/java/org/team2059/Wonko/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "WonkoTheSane";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "ce7856675acf4c2a6e7c80a35145fb1a8f7cfc6d";
-  public static final String GIT_DATE = "2025-04-02 20:57:23 EDT";
+  public static final int GIT_REVISION = 64;
+  public static final String GIT_SHA = "0955d057c1100f096ff558ea31df2a29a6ce4442";
+  public static final String GIT_DATE = "2025-04-03 18:58:51 EDT";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-04-03 18:36:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1743719762107L;
+  public static final String BUILD_DATE = "2025-04-03 19:02:53 EDT";
+  public static final long BUILD_UNIX_TIME = 1743721373262L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
