887bb4851480589d767461a87bb6b755e834f368
Ryan Fortner <ryankfortner@gmail.com>
2025-10-06 09:44:18 -0400

    Update gitignore

--- src/main/java/org/team2059/Wonko/BuildConstants.java
+++ src/main/java/org/team2059/Wonko/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "WonkoTheSane";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 72;
-  public static final String GIT_SHA = "08a68b28a8872e67253fa07da82c4c3a0735d591";
-  public static final String GIT_DATE = "2025-08-03 18:06:34 EDT";
-  public static final String GIT_BRANCH = "25offseason";
-  public static final String BUILD_DATE = "2025-08-04 19:04:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1754348669899L;
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "7dbf211d691ba04d1a7265f106b040136cba7e82";
+  public static final String GIT_DATE = "2025-10-06 09:22:28 EDT";
+  public static final String GIT_BRANCH = "thor";
+  public static final String BUILD_DATE = "2025-10-06 09:32:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1759757561873L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
